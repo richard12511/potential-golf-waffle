fdcab889bd23091988955957335e090491940946
richard12511 <richardnew12511@gmail.com>
2016-07-16 11:34:01 -0700

    all right triangles smaller than 11

--- app/Crab.hs
+++ app/Crab.hs
@@ -7,3 +7,5 @@ isRight :: (Integer, Integer, Integer) -> Bool
 isRight (a, b, c)
    | (a ^ 2) + (b ^ 2) == (c ^ 2) = True
    | otherwise                    = False
+
+allRightTrianglesSmallerThan11 = [(a, b, c) | a <- [1..10], b <- [1..10], c <- [1..10], isRight (a,b,c)]
