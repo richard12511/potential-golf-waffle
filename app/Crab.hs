db0803a70d7b7b38cbebcc770c78e943a5792c9c
richard12511 <richardnew12511@gmail.com>
2016-07-16 11:49:47 -0700

    all right triangles smaller than 11 with perimeter 24

--- app/Crab.hs
+++ app/Crab.hs
@@ -7,3 +7,4 @@ perimeter :: (Integer, Integer, Integer) -> Integer
 perimeter (a, b, c) = a + b + c
 
 allRightTrianglesSmallerThan11 = [(a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], isRight (a,b,c)]
+allRightTrianglesSmallerThan11AndPerimeter24 = [(a, b, c) | (a, b, c) <- allRightTrianglesSmallerThan11, perimeter (a,b,c) == 24]
