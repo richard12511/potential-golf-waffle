fb32791bedf1d6122ca54d6924c5a0dc2293a590
richard12511 <richardnew12511@gmail.com>
2016-07-15 17:14:25 -0700

    refactor

--- app/Golf.hs
+++ app/Golf.hs
@@ -6,7 +6,7 @@ every n xs = case drop (n-1) xs of
 buildList :: [a] -> [[a]] -> [[a]]
 buildList xs xsList 
    | (length xsList) < (length xs) = buildList xs (xsList ++ [(every (length xsList + 1) xs)])                 
-   | otherwise                = xsList
+   | otherwise                     = xsList
 
 skips :: [a] -> [[a]]
 skips xs = buildList xs []
