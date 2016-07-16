d611cb7f621885007dd33a3594955e99139a4885
richard12511 <richardnew12511@gmail.com>
2016-07-16 11:47:24 -0700

    added perimeter function

--- app/Crab.hs
+++ app/Crab.hs
@@ -3,4 +3,7 @@ isRight (a, b, c)
    | (a ^ 2) + (b ^ 2) == (c ^ 2) = True
    | otherwise                    = False
 
+perimeter :: (Integer, Integer, Integer) -> Integer
+perimeter (a, b, c) = a + b + c
+
 allRightTrianglesSmallerThan11 = [(a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], isRight (a,b,c)]
