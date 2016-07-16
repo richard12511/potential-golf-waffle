e35e18b7100c19c04669be0eead9507b53629e0b
richard12511 <richardnew12511@gmail.com>
2016-07-16 11:42:15 -0700

    added constraint that side (a < b < c)

--- app/Crab.hs
+++ app/Crab.hs
@@ -3,4 +3,4 @@ isRight (a, b, c)
    | (a ^ 2) + (b ^ 2) == (c ^ 2) = True
    | otherwise                    = False
 
-allRightTrianglesSmallerThan11 = [(a, b, c) | a <- [1..10], b <- [1..10], c <- [1..10], isRight (a,b,c)]
+allRightTrianglesSmallerThan11 = [(a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], isRight (a,b,c)]
