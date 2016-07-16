c568cd78eade8ca65464ff41e08dc4790cfc0c10
richard12511 <richardnew12511@gmail.com>
2016-07-16 11:24:40 -0700

    added function for testing whether or night a triangle is a right triangle

--- app/Crab.hs
+++ app/Crab.hs
@@ -2,3 +2,8 @@ allSidesSmallerThan10 :: (Integer, Integer, Integer) -> Bool
 allSidesSmallerThan10 (a,b,c)
    | (a < 10) && (b < 10) && (c < 10) = True
    | otherwise                        = False
+
+isRight :: (Integer, Integer, Integer) -> Bool
+isRight (a, b, c)
+   | (a ^ 2) + (b ^ 2) == (c ^ 2) = True
+   | otherwise                    = False
