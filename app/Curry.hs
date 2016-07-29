8144814fbad12feece5bac6ee52ca33f3b4e3ee8
richard12511 <richardnew12511@gmail.com>
2016-07-29 15:34:31 -0700

    added zipWith'

--- app/Curry.hs
+++ app/Curry.hs
@@ -11,3 +11,11 @@ zip' :: [a] -> [b] -> [(a,b)]
 zip' [] _ = []
 zip' _ [] = []
 zip' (x:xs) (y:ys) = (x, y) : zip' xs ys
+
+tupify :: a -> b -> (a, b)
+tupify x y = (x, y)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
