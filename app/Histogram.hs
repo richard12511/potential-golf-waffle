805bc554639c51bbca1cfde24b674d46245380c5
richard12511 <richardnew12511@gmail.com>
2016-09-05 19:59:12 -0500

    added countToString function

--- app/Histogram.hs
+++ app/Histogram.hs
@@ -1,15 +1,20 @@
 import Data.List
+import Data.Char
 
-countsOf :: Integer -> [Integer] -> Integer
+countsOf :: Int -> [Int] -> Int
 countsOf _ [] = 0
 countsOf x (y:ys)
    | x == y    = 1 + countsOf x ys
    | otherwise = countsOf x ys
 
-counts :: [Integer] -> [(Integer, Integer)]
+counts :: [Int] -> [(Int, Int)]
 counts [] = []
 counts xs = nub $ map (\a -> (a, (countsOf a xs))) xs
 
---ex: (3,4) becomes 3 = * * * *
---ex: (4,3) becomes 4 = * * *
-countToString :: (Integer, Integer) -> String
+numToStars :: Int -> String
+numToStars 0 = ""
+numToStars x = ' ' : '*' : (numToStars $ x - 1)
+
+countToString :: (Int, Int) -> String
+countToString (num, count) =
+  intToDigit num : ' ' : '=' : numToStars count
