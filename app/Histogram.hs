0364c98b85ed0ed8df80d70c5c559bef1ac131f6
richard12511 <richardnew12511@gmail.com>
2016-09-05 19:46:50 -0500

    added countToString definition

--- app/Histogram.hs
+++ app/Histogram.hs
@@ -9,3 +9,7 @@ countsOf x (y:ys)
 counts :: [Integer] -> [(Integer, Integer)]
 counts [] = []
 counts xs = nub $ map (\a -> (a, (countsOf a xs))) xs
+
+--ex: (3,4) becomes 3 = * * * *
+--ex: (4,3) becomes 4 = * * *
+countToString :: (Integer, Integer) -> String
