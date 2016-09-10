db4dd62c6d9527071d5a6280c2eabcea958eaae7
richard12511 <richardnew12511@gmail.com>
2016-09-09 19:36:06 -0500

    refactored addSpaces and addStars to addChars

--- app/Histogram.hs
+++ app/Histogram.hs
@@ -11,17 +11,13 @@ counts :: [Int] -> [(Int, Int)]
 counts [] = []
 counts xs = nub $ map (\a -> (a, (countsOf a xs))) xs
 
-addSpaces :: Int -> String
-addSpaces 0 = []
-addSpaces x = ' ' : ' ' : addSpaces (x - 1)
-
-addStars :: Int -> String
-addStars 0 = []
-addStars x = ' ' : '*' : addStars (x - 1)
+addChars :: Char -> Int -> String
+addChars _ 0 = []
+addChars char x = ' ' : char : addChars char (x - 1)
 
 numToStars :: Int -> String
 numToStars 0 = ""
-numToStars x = addSpaces (9 - x) ++ addStars (x)
+numToStars x = addChars ' ' (9 - x) ++ addChars '*' (x)
 
 countToString :: (Int, Int) -> String
 countToString (num, count) =
