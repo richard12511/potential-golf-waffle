d6283da33cf2790aee52f995d3b27b7d36c47f94
richard12511 <richardnew12511@gmail.com>
2016-09-09 19:47:07 -0500

    added comment for counts function

--- app/Histogram.hs
+++ app/Histogram.hs
@@ -7,6 +7,8 @@ countsOf x (y:ys)
    | x == y    = 1 + countsOf x ys
    | otherwise = countsOf x ys
 
+--counts should give counts of all nums 1-9, not just the ones present in the List
+--ex: counts [1,1,2] should give [(2,1), (1,2), (0,3), (0,4), (0,5) ...]
 counts :: [Int] -> [(Int, Int)]
 counts [] = []
 counts xs = nub $ map (\a -> (a, (countsOf a xs))) xs
@@ -17,7 +19,7 @@ addChars char x = ' ' : char : addChars char (x - 1)
 
 numToStars :: Int -> String
 numToStars 0 = ""
-numToStars x = addChars ' ' (9 - x) ++ addChars '*' (x)
+numToStars x = addChars '*' (x) ++ addChars ' ' (9 - x)
 
 countToString :: (Int, Int) -> String
 countToString (num, count) =
