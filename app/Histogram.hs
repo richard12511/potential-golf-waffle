8627accbd9c22e9d3695cf00defeed1254ca10af
richard12511 <richardnew12511@gmail.com>
2016-09-13 18:46:50 -0500

    updated counts to use all numbers between 1 and 9, not just the numbers present in the list

--- app/Histogram.hs
+++ app/Histogram.hs
@@ -7,11 +7,9 @@ countsOf x (y:ys)
    | x == y    = 1 + countsOf x ys
    | otherwise = countsOf x ys
 
---counts should give counts of all nums 1-9, not just the ones present in the List
---ex: counts [1,1,2] should give [(2,1), (1,2), (0,3), (0,4), (0,5) ...]
 counts :: [Int] -> [(Int, Int)]
 counts [] = []
-counts xs = nub $ map (\a -> (a, (countsOf a xs))) xs
+counts xs = nub $ map (\a -> (a, (countsOf a xs))) (xs `union` [1..9])
 
 addChars :: Char -> Int -> String
 addChars _ 0 = []
